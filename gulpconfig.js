2b97c8b1a370fb0e29340e6189937744fa04dc89
shenjunjian <867427795@qq.com>
2020-10-10 17:20:24 +0800

    调整

--- gulpconfig.js
+++ gulpconfig.js
@@ -7,7 +7,7 @@
  * @Autor: popup
  * @Date: 2020-09-29 17:00:49
  * @LastEditors: popup
- * @LastEditTime: 2020-10-10 17:01:06
+ * @LastEditTime: 2020-10-10 17:19:40
  */
 
 
@@ -23,7 +23,9 @@ module.exports = {
   },
   // 需要编译的别名js文件，本工具默认监听ybf.js
   buildJsUrl: ['./pages/**/ybf.js'],
-  // 需要编译的scss文件，如果是abc.scss 则编译成abc.scss // 如果不写则默认监听所有除node_modules依赖的全部scss
+  // 这里建议写好文件的路径，方便gulp减少文件的监听
+
+  // 需要编译的scss文件，如果是abc.scss 则编译成abc.scss
   buildScssUrl: ['./pages/**/*.scss', './components/**/*.scss']
 
 }
