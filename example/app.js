9f519b7ea88b66ddea77291dc45ba6122a2a65cd
shenjunjian <867427795@qq.com>
2020-10-10 17:42:37 +0800

    新增example

---
+++ example/app.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 这是文件代码注释
+ * @Version: 1.0
+ * @Autor: popup
+ * @Date: 2020-10-10 17:35:06
+ * @LastEditors: popup
+ * @LastEditTime: 2020-10-10 17:35:34
+ */
+
+App({
+  onLaunch: () => {
+  },
+  onShow() {
+  },
+  globalData: {}
+})
